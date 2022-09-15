986e87e170321aaa1aa75e20f6b70c03c0ec4d74
Etereke <98815314+Etereke@users.noreply.github.com>
2022-09-15 09:14:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class GCTest
+    {
+        static int numOfThis = 0;
+        public static int maxNumOfThis = 0;
+        public GCTest()
+        {
+            numOfThis++;
+            Console.WriteLine(GCTest.numOfThis);
+            if (numOfThis > maxNumOfThis)
+            {
+                maxNumOfThis = numOfThis;
+            }
+        }
+
+        ~GCTest()
+        {
+            numOfThis--;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 500000; i++)
+            {
+                new GCTest();
+            }
+            //while (true)
+            //{
+            //    new GCTest();
+            //}
+            Console.WriteLine("The garbage collector started at object #" + GCTest.maxNumOfThis);
+        }
+    }
+}
